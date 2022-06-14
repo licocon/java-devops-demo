acd4e65a90e03d326e6e29fc9c70ffbbc0b6a68e
licocon <licocon@aliyun.com>
2022-06-14 16:27:51 +0800

    init

---
+++ src/main/java/com/example/devops/JavaDevopsDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author licocon
+ */
+@SpringBootApplication
+public class JavaDevopsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopsDemoApplication.class, args);
+    }
+
+}
